d38ff6a3cb2797d43d05bcca79a2f391ceb8dffd
Lucas Araujo <Lucas_santos.s@outlook.com>
2022-10-31 21:14:56 -0300

    up

---
+++ script.js
@@ -0,0 +1,12 @@
+function menu(){
+    document.getElementById("abrir").style.width ="250px";
+    document.getElementById("conteudo").style.marginLeft ="250px"
+    document.getElementById("alunos").style.marginLeft ="250px"
+    document.getElementById("menu").style.display = "none";
+}
+function fechar(){
+    document.getElementById("abrir").style.width ="0px"
+    document.getElementById("conteudo").style.marginLeft ="0px"
+    document.getElementById("alunos").style.marginLeft ="0px"
+    document.getElementById("menu").style.display = "inline";
+}
